ede933af5ae7143f5127ba45f342c07e3a477729
smc6548 <seancliff01@gmail.com>
2020-02-09 00:23:23 -0500

    Some of the basic site and routing written

---
+++ routes/remote.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+
+function getMongoData() {
+    console.log("stuff");
+}
+
+var getMongoDataMiddleware = function(req, res, next) {
+    if (req.params.dorm && req.params.door) {
+        req.mongoData = getMongoData();
+        return next();
+    } else {
+        return next();
+    }
+}
+
+router.get('/status/?dorm/?door', getMongoData, (req, res, next) => {
+
+    console.log(dorm);
+    res.json(`Elevator status of elevator number ${req.params.door} in ${req.params.dorm}`);
+});
+
+router.post('/update', (req, res) => {
+    console.log(req.params);
+});
