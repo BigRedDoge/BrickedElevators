0d814867c82d52d2e2ac838bdb7cb743a5881bfc
smc6548 <seancliff01@gmail.com>
2020-02-09 12:06:19 -0500

    fixed

--- getBuildings.js
+++ getBuildings.js
@@ -2,40 +2,34 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://dbUser:2S1tQbxl0YXv8Xth@brickedelevator-4g5z0.mongodb.net/test?retryWrites=true&w=majority"
 
 
-var con = new Promise((resolve) => {
-	MongoClient.connect(url, function(err, db) {
-		if(err) throw err;
-		resolve(db.db("brickedElevator"));
-	});
-}).then((dbo) => {
-	function getBuilding(building){
-		return new Promise((resolve, reject) => {
-			MongoClient.connect(url, function(err, db) {
-				if(err) throw err;
-				var dbo = db.db("brickedElevator");
-				dbo.collection("brickedElevator").findOne({"building": building}, function(err, result) {
-					if (err) throw err;
-					resolve(result);
-				});
-			});
-		});
-	}
-	
-	function getBuildings() {
-		return new Promise((resolve, reject) => {
-			MongoClient.connect(url, function(err, db) {
+function getBuilding(building) {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, function (err, db) {
+			if (err) throw err;
+			var dbo = db.db("brickedElevator");
+			dbo.collection("brickedElevator").findOne({ "building": building }, function (err, result) {
 				if (err) throw err;
-				var dbo = db.db("brickedElevator");
-				dbo.collection("brickedElevator").find({}, function(err, result) {
-					if (err) throw err;
-					resolve(result);
-				});
+				resolve(result);
 			});
 		});
-	}
-	
-	getBuildings().then((res) => console.log(res));
+	});
+}
 
-	
-});
+function getBuildings() {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, function (err, db) {
+			if (err) throw err;
+			var dbo = db.db("brickedElevator");
+			dbo.collection("brickedElevator").find({}, function (err, result) {
+				if (err) throw err;
+				resolve(result);
+			});
+		});
+	});
+}
+
+getBuildings().then((res) => console.log(res));
+
+
+
 
