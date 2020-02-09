9e68f91b1b5620d3468045b2c9793948ae333d4e
kmt3271 <kmt3271@g.rit.edu>
2020-02-09 02:04:36 -0500

    Tried some more stuff to get getBuildings to work didn't succeed tho

--- getBuildings.js
+++ getBuildings.js
@@ -16,15 +16,18 @@ async function getBuildings(){
 	var MongoClient = require('mongodb').MongoClient;
 	var url = "mongodb+srv://dbUser:2S1tQbxl0YXv8Xth@brickedelevator-4g5z0.mongodb.net/test?retryWrites=true&w=majority"
 
-	MongoClient.connect(url, function(err, db){
-		if(err) throw err;
-		var dbo = db.db("brickedElevator");
-		dbo.collection("brickedElevator").findOne({"building": building}, function(err, result){
-			if(err) throw err;
-			db.close();
-			return result;
-		});
-	});
+	var client = await MongoClient.connect(url, {useNewUrlParser: true}).catch(err => { console.log(err);});
+	try{
+		var dbo = client.db("brickedElevator");
+		var collection = dbo.collection("brickedElevator");
+		var result = await collection.find({}).toArray();
+		return result;
+	}catch (err){
+		console.log(err);
+		console.log("AHHHHHH");
+	}finally{
+		client.close();
+	}
 }
-var building = getBuilding("Gibson");
+var building = getBuildings();
 console.log(building);
