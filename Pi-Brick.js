c3f65fddeb56fcfd60532869e968713f1aa2d2c5
smc6548 <seancliff01@gmail.com>
2020-02-09 07:21:46 -0500

    Added WebSocket functionality to raspberry pi

--- Pi-Brick.js
+++ Pi-Brick.js
@@ -1,49 +1,70 @@
 var http = require('http');
 var fs = require('fs');
+var WebSocket = require('ws');
+
 var Gpio = require('onoff').Gpio;
-var LED = [new Gpio(2,'out'),new Gpio(8,'out')];
-var setButton = [new Gpio(3,'in','rising',{debounceTimeout:50}),new Gpio(5,'in','rising',{debounceTimeout:50})];
-var resetButton = [new Gpio(4,'in','rising',{debounceTimeout:50}),new Gpio(6,'in','rising',{debounceTimeout:50})];
-var broken = [0,0];
-var elevators = ['NRH1','NRH2']
+var LED = [new Gpio(2, 'out'), new Gpio(8, 'out')];
+var setButton = [new Gpio(3, 'in', 'rising', { debounceTimeout: 50 }),
+new Gpio(5, 'in', 'rising', { debounceTimeout: 50 })];
+var resetButton = [new Gpio(4, 'in', 'rising', { debounceTimeout: 50 }),
+new Gpio(6, 'in', 'rising', { debounceTimeout: 50 })];
+var broken = [0, 0];
+var elevators = ['NRH1', 'NRH2']
+var building = 'NRH';
 
-const options = {
-    port:80,
-    host:'',
-    method:'CONNECT',
-    path:''
-};
-
-const req = http.request(options);
-req.end();
-
-req.on('connect',(res,socket,head)=>{
-    for(var i=0;i<2;i++){
-        req.on(elevators[i],(state)=>{
-            broken[i] = state;
-            LED[i].writeSync(state);
-        });
-        setButton[i].watch(function(err,value){
-            if(err){
-                console.error('Setting Error');
-                return;
-            }
-            else if(!broken[i]){
-                req.emit(elevators[i],1);
-                broken=1;
-                LED[i].writeSync(1);
-            }
-        });
-        resetButton[i].watch(function(err,value){
-            if(err){
-                console.error('Resetting Error');
-                return;
-            }
-            else if(broken[i]){
-                req.emit(elevators[i],0);
-                broken=0;
-                LED[i].writeSync(0);
-            }
-        });
-    }
+var ws = new WebSocket('ws://192.168.43.187/:3030', {
+    perMessageDeflate: false
 });
+
+ws.on('open', function open() {
+    console.log("Connected to Server!");
+    ws.send("Remote Pi Connected");
+    monitorInputs
+});
+
+ws.on('update', function (data) {
+    var info = JSON.parse(data);
+    var door = (info.door).parseInt();
+    var status = (info.status).parseInt();
+    broken[door] = status
+    LED[door].writeSync(status)
+});
+
+function sendStatus(door, status) {
+    var data = {
+        "building": building,
+        "elevator_door": door,
+        "broken": status
+    }
+    ws.send(JSON.stringify(data));
+}
+
+function monitorInputs() {
+    setButton[0].watch(function (err, value) {
+        btnHandler(0, 1);
+    });
+    setButton[1].watch(function (err, value) {
+        btnHandler(1, 1);
+    });
+
+    resetButton[0].watch(function (err, value) {
+        btnHandler(0, 0);
+    });
+    resetButton[1].watch(function (err, value) {
+        btnHandler(1, 0);
+    });
+}
+
+
+function btnHandler(door, status) {
+    if (err) {
+        console.error('Setting Error');
+        return;
+    }
+    if (broken[door]) {
+        broken[door] = status;
+        sendStatus(door, status)
+        LED[!status].writeSync(status);
+    }
+}
+
